135a2909ba7b95891f261a66d502963c4fe27cc6
sershishkov <ser.shishkov@gmail.com>
2026-04-16 18:25:06 +0300

    start header

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,5 +1,43 @@
-import React from 'react';
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    const setHidration = () => {
+      setIsClient(true);
+    };
+    setHidration();
+  }, []);
+
+  if (!isClient) {
+    return <p>loading</p>;
+  }
+  return (
+    <Grid
+      container
+      direction='column'
+      sx={{
+        margin: `0 auto`,
+        maxWidth: `1200px`,
+      }}
+    >
+      <Grid xs={12}>
+        <Typography variant='h6' align='center'>
+          Bormin Rénovation, Rue du Clos 8, 1800 VEVEY, Suisse, teL:
+          +41765314652, borminart@gmail.com
+        </Typography>
+      </Grid>
+      <Grid xs={12}>
+        <Typography variant='h6' align='center'>
+          IBAN: CH 29 0026 6266 1373 3540, VISA Card 5351 4602 1314 4652
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,5 +1,60 @@
-import React from 'react';
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 export const Header = () => {
-  return <div>Header</div>;
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    const setHidration = () => {
+      setIsClient(true);
+    };
+    setHidration();
+  }, []);
+
+  if (!isClient) {
+    return <p>loading</p>;
+  }
+  return (
+    <Grid
+      container
+      sx={{
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+        width: `100%`,
+        position: 'fixed',
+        top: 0,
+        zIndex: 100,
+      }}
+    >
+      <Grid size={4}>
+        <Link href='/'>
+          <Typography variant='h6' align='center'>
+            Bormin Rénovation,
+          </Typography>
+        </Link>
+      </Grid>
+      <Grid size={4}>
+        <Typography variant='h6' align='center'>
+          Rue du Clos 8, 1800 VEVEY, Suisse
+        </Typography>
+      </Grid>
+      <Grid size={4}>
+        <Typography variant='h6' align='center'>
+          <Link href='tel:+41765314652' underline='hover'>
+            teL: +41765314652
+          </Link>
+          {'   '}
+          <Link href='mailto:borminart@gmail.com' underline='hover'>
+            {'   '}borminart@gmail.com
+          </Link>
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
